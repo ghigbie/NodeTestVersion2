97406b9769e1050dc1c57b90a90031af1a93646a
George Higbie <gehigbie@gmail.com>
2017-04-27 18:22:02 -0700

    Add new test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,27 @@
+const expect = require("expect");
+
+//user created imports
 const utils = require("./utils");
+
 
 it("should add two numbers", () => {
     let res = utils.add(33, 11);
-    if(res != 44){
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+
+    expect(res).toBe(44).toBeA("number");
+    // if(res != 44){
+    //     throw new Error(`Expected 44, but got ${res}.`);
+    // }
 });
 
 it("should square a number", () => {
     let res = utils.square(10);
-    if(res != 100){
-        throw new Error(`Expected 100, but got ${res}.`);
-    }
+
+    expect(res).toBe(100).toBeA("number");
+    // if(res != 100){
+    //     throw new Error(`Expected 100, but got ${res}.`);
+    // }
+});
+
+it("should return some value", () => {
+    expect(12).toNotBe(11);
 });
