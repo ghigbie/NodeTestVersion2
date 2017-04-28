4c34a08405b582bfb393c8dae761e9b1e815fbe0
George Higbie <gehigbie@gmail.com>
2017-04-27 23:39:48 -0700

    Add root route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express"),
       app     = express();
 
 let port = 3010;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to the landing page</h1>");
+    console.log("You are in the landing page");
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
 });
