e02371788e8eba97514b24d9be1eb0c23a8f0d3a
George Higbie <gehigbie@gmail.com>
2017-04-29 10:19:00 -0700

    Add embedded test blocks

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,35 +4,39 @@ const expect = require("expect");
 const utils = require("./utils");
 
 describe("Utils", () =>{
-    it("should add two numbers", () => {
-        let res = utils.add(33, 11);
+    describe("#add", () => {
+        it("should add two numbers", () => {
+            let res = utils.add(33, 11);
 
-        expect(res).toBe(44).toBeA("number");
-        // if(res != 44){
-        //     throw new Error(`Expected 44, but got ${res}.`);
-        // }
-    });
+            expect(res).toBe(44).toBeA("number");
+            // if(res != 44){
+            //     throw new Error(`Expected 44, but got ${res}.`);
+            // }
+        });
 
-    it("should asyncAdd two numbers", (done) => {
-        utils.asyncAdd(44, 11, (sum) =>{
-            expect(sum).toBe(55).toBeA("number");
-            done();
+        it("should asyncAdd two numbers", (done) => {
+            utils.asyncAdd(44, 11, (sum) =>{
+                expect(sum).toBe(55).toBeA("number");
+                done();
+            });
         });
     });
 
-    it("should square a number", () => {
-        let res = utils.square(10);
+    describe("#square", () => {
+        it("should square a number", () => {
+            let res = utils.square(10);
 
-        expect(res).toBe(100).toBeA("number");
-        // if(res != 100){
-        //     throw new Error(`Expected 100, but got ${res}.`);
-        // }
-    });
+            expect(res).toBe(100).toBeA("number");
+            // if(res != 100){
+            //     throw new Error(`Expected 100, but got ${res}.`);
+            // }
+        });
 
-    it("should asyncSquare a number", (done) => {
-        utils.asyncSquare(10, (result) => {
-            expect(result).toBe(100).toBeA("number");
-            done();
+        it("should asyncSquare a number", (done) => {
+            utils.asyncSquare(10, (result) => {
+                expect(result).toBe(100).toBeA("number");
+                done();
+            });
         });
     });
 });
