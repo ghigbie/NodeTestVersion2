bd41519d0c42a24606d51eea0d502cf5c38b9487
George Higbie <gehigbie@gmail.com>
2017-04-29 10:26:03 -0700

    Use describe in server

--- server/server.test.js
+++ server/server.test.js
@@ -3,31 +3,39 @@ const request = require("supertest"),
 
 const app = require("./server").app;
 
-it("should return \"<h1>Welcome to the landing page</h1>\" response", (done) =>{
-    request(app)
-        .get("/")
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: "Page not found"
-            });
-        })
-        // .expect({
-        //     error: "Page not found"
-        // })
-        //.expect("<h1>Welcome to the landing page</h1>")
-        .end(done);
-});
+describe("Server", () => {
 
-it("should return an array of users", (done) => {
-    request(app)
-        .get("/users")
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: "Billy Bobby",
-                age: 25
-            });
-        })
-        .end(done);
+    describe("GET /", () => {
+        it("should return \"<h1>Welcome to the landing page</h1>\" response", (done) =>{
+            request(app)
+                .get("/")
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: "Page not found"
+                    });
+                })
+                // .expect({
+                //     error: "Page not found"
+                // })
+                //.expect("<h1>Welcome to the landing page</h1>")
+                .end(done);
+        });
+    });
+
+    describe("GET /users", () => {
+        it("should return an array of users", (done) => {
+            request(app)
+                .get("/users")
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: "Billy Bobby",
+                        age: 25
+                    });
+                })
+                .end(done);
+        });
+    });
+
 });
