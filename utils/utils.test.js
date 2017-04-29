a2054f4fc3c15f19735f09ed1f9401a975ded852
George Higbie <gehigbie@gmail.com>
2017-04-29 10:14:57 -0700

    Add describe blocks to tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,73 +3,78 @@ const expect = require("expect");
 //user created imports
 const utils = require("./utils");
 
+describe("Utils", () =>{
+    it("should add two numbers", () => {
+        let res = utils.add(33, 11);
 
-it("should add two numbers", () => {
-    let res = utils.add(33, 11);
+        expect(res).toBe(44).toBeA("number");
+        // if(res != 44){
+        //     throw new Error(`Expected 44, but got ${res}.`);
+        // }
+    });
 
-    expect(res).toBe(44).toBeA("number");
-    // if(res != 44){
-    //     throw new Error(`Expected 44, but got ${res}.`);
-    // }
-});
+    it("should asyncAdd two numbers", (done) => {
+        utils.asyncAdd(44, 11, (sum) =>{
+            expect(sum).toBe(55).toBeA("number");
+            done();
+        });
+    });
 
-it("should asyncAdd two numbers", (done) => {
-    utils.asyncAdd(44, 11, (sum) =>{
-        expect(sum).toBe(55).toBeA("number");
-        done();
+    it("should square a number", () => {
+        let res = utils.square(10);
+
+        expect(res).toBe(100).toBeA("number");
+        // if(res != 100){
+        //     throw new Error(`Expected 100, but got ${res}.`);
+        // }
+    });
+
+    it("should asyncSquare a number", (done) => {
+        utils.asyncSquare(10, (result) => {
+            expect(result).toBe(100).toBeA("number");
+            done();
+        });
     });
 });
 
-it("should square a number", () => {
-    let res = utils.square(10);
-
-    expect(res).toBe(100).toBeA("number");
-    // if(res != 100){
-    //     throw new Error(`Expected 100, but got ${res}.`);
-    // }
-});
-
-it("should asyncSquare a number", (done) => {
-    utils.asyncSquare(10, (result) => {
-        expect(result).toBe(100).toBeA("number");
-        done();
+describe("Set Name", () =>{
+    it("set firstName and lastName", () => {
+        let user = {location: "Santa Fe", age: 25}
+        let fullName = "Billy Bobby";
+        let res = utils.setName(user, fullName);
+        expect(res).toEqual({
+            age: 25,
+            firstName: "Billy",
+            lastName: "Bobby",
+            location: "Santa Fe"
+        }).toBeA(Object);
     });
 });
 
-it("set firstName and lastName", () => {
-    let user = {location: "Santa Fe", age: 25}
-    let fullName = "Billy Bobby";
-    let res = utils.setName(user, fullName);
-    expect(res).toEqual({
-        age: 25,
-        firstName: "Billy",
-        lastName: "Bobby",
-        location: "Santa Fe"
-    }).toBeA(Object);
-});
+describe("Playground", () => {
+    it("should return some value", () => {
+        expect(12).toNotBe(11);
+    });
 
-it("should return some value", () => {
-    expect(12).toNotBe(11);
-});
+    it("should compare two objects", () => {
+        expect({name: "mike"}).toNotEqual({name: "Mike"}); //used for equality with objects and arrays
+    });
 
-it("should compare two objects", () => {
-    expect({name: "mike"}).toNotEqual({name: "Mike"}); //used for equality with objects and arrays
-});
+    it("should examine an array", () => {
+        expect([1, 2, 3]).toInclude(5); //looks at values of arrays
+    });
 
-it("should examine an array", () => {
-    expect([1, 2, 3]).toInclude(5); //looks at values of arrays
-});
+    it("should examine an array", () => {
+        expect([1, 2, 3]).toExclude(6); //another array testing method
+    });
 
-it("should examine an array", () => {
-    expect([1, 2, 3]).toExclude(6); //another array testing method
-});
-
-it("should examine an object", () => {
-    expect({
-        name: "Kristi",
-        age: 25,
-        location: "Santa Fe"
-    }).toExclude({
-        age: 24
-    });//looks to see if object has a value
+    it("should examine an object", () => {
+        expect({
+            name: "Kristi",
+            age: 25,
+            location: "Santa Fe"
+        }).toExclude({
+            age: 24
+        });//looks to see if object has a value
+    });
 });
