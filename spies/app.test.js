dd850b15181775a8c89685ed8eb3a40d4081e709
George Higbie <gehigbie@gmail.com>
2017-04-30 01:43:07 -0700

    Add another test case with spy

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,6 +1,13 @@
-const expect = require("expect");
+const expect = require("expect"),
+      rewire = require("rewire");
+
+let app = rewire("./app");
 
 describe("App", () => {
+    let db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__("db", db);
 
     it("should call the spy correctly", () => {
         let spy = expect.createSpy();
@@ -12,5 +19,13 @@ describe("App", () => {
         let spy = expect.createSpy();
         spy("George", 25);
         expect(spy).toHaveBeenCalledWith("George", 25);
+    });
+
+    it("should call saveuser with user object", () => {
+        let email = "mail@amail.com";
+        let password = "123abc";
+
+        app.handleSignup(email, password);
+        expect(db.saveUser).toHaveBeenCalledWith({email, password});
     });
 });
