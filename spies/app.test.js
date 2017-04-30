0f452cc9beb68a3e48145f4e88edf49e9d0abb70
George Higbie <gehigbie@gmail.com>
2017-04-30 00:47:45 -0700

    Add additional test case

--- spies/app.test.js
+++ spies/app.test.js
@@ -2,8 +2,15 @@ const expect = require("expect");
 
 describe("App", () => {
 
-    it("It should call the spy correctly", () => {
+    it("should call the spy correctly", () => {
         let spy = expect.createSpy();
         spy();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it("should call the spy correctly with arguments", () => {
+        let spy = expect.createSpy();
+        spy("George", 25);
+        expect(spy).toHaveBeenCalledWith("George", 25);
     });
 });
