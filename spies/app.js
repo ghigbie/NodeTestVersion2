9278500f57e3fc3a84ab1793361aeed4f5df987e
George Higbie <gehigbie@gmail.com>
2017-04-29 22:20:29 -0700

    Call saveuser function

--- spies/app.js
+++ spies/app.js
@@ -1,5 +1,6 @@
 module.exports.handleSignup = (email, password) => {
     //Check if email already exisits
     //Save the user to the database
+    db.saveUser({ email, password }); //ES6 use of object with names the sames as the property
     //Send the welcome email
 };
